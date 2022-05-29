5961a1f1e1486156613ba99fffa0f315277adb76
BooraAnilKumar <anilkumarboora@hotmail.com>
2022-05-29 16:56:42 -0700

    fixed test

--- myfirst_dbt/tests/asser_under_10_percent_null.sql
+++ myfirst_dbt/tests/asser_under_10_percent_null.sql
@@ -2,4 +2,4 @@ select
   sum(case when id is null then 1 else 0 end)/count(*) as total_null
 from
   {{ref('my_first_dbt_model')}}
-having um(case when id is null then 1 else 0 end)/count(*) <.1
+having um(case when id is null then 1 else 0 end)/count(*) <.5
